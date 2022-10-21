76022432d0f13acef39fe61e19d15a5e163f17ad
Rocha134 <rochapakito@gmail.com>
2022-10-21 04:34:53 -0500

    City-1

--- main.cpp
+++ main.cpp
@@ -13,6 +13,92 @@ int Next[MAXN][MAXN];
 
 int ary[10][10],completed[10],n,cost=0;
 
+//Function to convert numbers to letters
+string numberToLetter(int number){
+    string letter;
+    switch (number) {
+        case 0:
+            letter = "A";
+            break;
+        case 1:
+            letter = "B";
+            break;
+        case 2:
+            letter = "C";
+            break;
+        case 3:
+            letter = "D";
+            break;
+        case 4:
+            letter = "E";
+            break;
+        case 5:
+            letter = "F";
+            break;
+        case 6:
+            letter = "G";
+            break;
+        case 7:
+            letter = "H";
+            break;
+        case 8:
+            letter = "I";
+            break;
+        case 9:
+            letter = "J";
+            break;
+        case 10:
+            letter = "K";
+            break;
+        case 11:
+            letter = "L";
+            break;
+        case 12:
+            letter = "M";
+            break;
+        case 13:
+            letter = "N";
+            break;
+        case 14:
+            letter = "O";
+            break;
+        case 15:
+            letter = "P";
+            break;
+        case 16:
+            letter = "Q";
+            break;
+        case 17:
+            letter = "R";
+            break;
+        case 18:
+            letter = "S";
+            break;
+        case 19:
+            letter = "T";
+            break;
+        case 20:
+            letter = "U";
+            break;
+        case 21:
+            letter = "V";
+            break;
+        case 22:
+            letter = "W";
+            break;
+        case 23:
+            letter = "X";
+            break;
+        case 24:
+            letter = "Y";
+            break;
+        case 25:
+            letter = "Z";
+            break;
+    }
+    return letter;
+}
+
 void takeInput()
 {
     int i,j;
@@ -86,7 +172,7 @@ void mincost(int city)
     if(ncity==999)
     {
         ncity=0;
-        cout<<ncity+1;
+        cout<<ncity;
         cost+=ary[city][ncity];
 
         return;
